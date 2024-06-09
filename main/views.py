e94df797e175545ce0e8def8498a7546db1e570b
Kunal-Shaw-097 <decodednature097@gmail.com>
2024-06-09 23:42:52 +0530

    minor changes

--- main/views.py
+++ main/views.py
@@ -32,6 +32,20 @@ def upload_csv(request):
             os.remove(CSV.csv.path)
             CSV.delete()
 
+            stats_dict = {}
+            for col in num_cols :
+                stats_dict[col] = {
+                    "Mean" : round(float(num_df[col].mean()),2),
+                    "Median": round(float(num_df[col].median()), 2),
+                    "Standard Deviation": round(float(num_df[col].std()), 2),
+                    "Missing Value Count": int(num_df[col].isna().sum()),
+                    "Max value": round(float(num_df[col].max()), 2), 
+                    "Min value": round(float(num_df[col].min()), 2),
+                    "Unique value count": int(num_df[col].unique().__len__()),
+                }
+
+            
+            request.session['stats_dict'] = stats_dict
             request.session['uploaded'] = True
             request.session['df'] = df.to_dict()
             request.session['num_cols'] = num_cols
@@ -67,17 +81,8 @@ def calculate_stats(request):
         stats_dict = {}
 
         for col in stat_cols :
-            stats_dict[col] = {
-                "Mean" : round(float(num_df[col].mean()),2),
-                "Median": round(float(num_df[col].median()), 2),
-                "Standard Deviation": round(float(num_df[col].std()), 2),
-                "Missing Value Count": int(num_df[col].isna().sum()),
-                "Max value": round(float(num_df[col].max()), 2), 
-                "Min value": round(float(num_df[col].min()), 2),
-                "Unique value count": int(num_df[col].unique().__len__()),
-            }
+            stats_dict[col] = request.session['stats_dict'][col]
 
-        request.session['stats_dict'] = stats_dict
         request.session['selected_columns'] = stat_cols
 
         context = {
@@ -117,14 +122,49 @@ def generate_plots(request):
         # else :
         #     html = "Column names can not be same"
 
-        fig = plt.figure(figsize=(12, 10))
+        fig, ax = plt.subplots(figsize=(12, 10))
+        rgba_color = (1/255, 75/255, 27/255, 0)  # RGBA color
+        ax.set_facecolor(rgba_color)
         plt.rcParams['font.size'] = 22
 
+        mean = request.session['stats_dict'][x_col]["Mean"]
+        median = request.session['stats_dict'][x_col]["Median"]
+
         if plot_type == "hist" :
-            plt.hist(df[x_col])
+            ax.hist(df[x_col], edgecolor='black')
+
+            # ax.axvline(mean, color='r', linestyle='dashed', linewidth=2, label=f'Mean: {mean}')
+            # ax.axvline(median, color='b', linestyle='dashed', linewidth=2, label=f'Median: {median}')
+
+            ax.plot(mean, 0, 'ro', label=f'Mean: {mean:.2f}')
+            ax.plot(median, 0, 'bs', label=f'Median: {median:.2f}')
+            
+            ax.set_xlabel(f"{x_col} (ranges)")
+            ax.set_ylabel("Frequency")
+            ax.set_title(f"Histogram for \"{x_col}\"")
+            ax.legend()
 
         elif plot_type == "box" :
-            plt.boxplot(df[x_col])
+            boxprops = dict(linestyle='-', linewidth=2, color='black')
+            medianprops = dict(linestyle='-', linewidth=2.5, color='blue')
+            meanprops = dict(marker='o', markerfacecolor='red', markersize=12)
+            ax.boxplot(df[x_col], patch_artist=True, boxprops=boxprops, medianprops=medianprops, showmeans=True, meanprops=meanprops)
+
+            # Set the title and labels
+            ax.set_title(f"Box plot of {x_col}", color='black')
+            ax.set_ylabel(x_col, color='black')
+
+            # Annotate mean and median
+            ax.annotate(f'Mean: {mean:.2f}', xy=(1, mean), xytext=(1.2, mean),
+                        arrowprops=dict(facecolor='red', shrink=0.05), color='red')
+            ax.annotate(f'Median: {median:.2f}', xy=(1, median), xytext=(0.6, median),
+                        arrowprops=dict(facecolor='blue', shrink=0.05), color='blue')
+
+            # Annotate Q1 and Q3
+            # ax.annotate(f'Q1: {q1:.2f}', xy=(1, q1), xytext=(1.1, q1),
+            #             arrowprops=dict(facecolor='green', shrink=0.05), color='green')
+            # ax.annotate(f'Q3: {q3:.2f}', xy=(1, q3), xytext=(1.1, q3),
+            #             arrowprops=dict(facecolor='green', shrink=0.05), color='green')
 
         else :
 
@@ -135,29 +175,36 @@ def generate_plots(request):
 
             bin_counts_list = bin_counts.tolist()
 
-            wedges, texts = plt.pie(bin_counts_list, startangle=90, shadow=True, explode=[0.1]*len(bin_counts_list))
+            wedges, texts = ax.pie(bin_counts_list, startangle=90, shadow=True, explode=[0.1]*len(bin_counts_list))
 
             total = sum(bin_counts_list)
             legend_labels = [f'{label}: {count} ({count/total:.1%})' for label, count in zip(bin_labels, bin_counts_list)]
 
-            plt.legend(wedges, legend_labels, title="Ranges", loc="center left", bbox_to_anchor=(1, 0, 0.5, 1))
+            ax.legend(wedges, legend_labels, title="Ranges", loc="center left", bbox_to_anchor=(1, 0, 0.5, 1))
 
             # Set the title
-            plt.title(f"Pie plot for \"{x_col}\"")
+            ax.set_title(f"Pie plot for \"{x_col}\"")
 
-            plt.tight_layout()
+        plt.tight_layout()
+        plt.plot()
 
         html = mpld3.fig_to_html(fig, figid='plot')
 
         #request.session['plotted_columns_types'] = [x_col, y_col, plot_type]
         request.session['plotted_columns_types'] = [x_col, plot_type]
         request.session['plot_html'] = html
+        
+        stats_dict = {}
+
+        for col in request.session.get('selected_columns',[]):
+            stats_dict[col] = request.session['stats_dict'][col]
+        
 
         context = {
             "csvform" : csvform,
             "column_form" : column_form,
             "top10" : top10,
-            "stats_dict" : request.session.get('stats_dict', []),
+            "stats_dict" : stats_dict,
             "uploaded" : request.session['uploaded'],
             "axis_select" : axis_select,
             "plot" : html,
